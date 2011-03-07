5ccf83d078c8d8b36362f92ee3a0e055d9d6f19b
Yacin Nadji <yacin@gatech.edu>
2011-03-06 20:25:45 -0500

    Catch analysis errors
    Occasionally echo nest analysis engine fails. We catch that error
    and skip the mp3 file. We also catch KeyboardInterrupt to make pausing
    it easier. Also, maxkey wasn't getting saved properly, so I added a finally
    block to always do that.
    
    This should probably be a few commits, but there's no way I'm going to really
    add/commit everything piecewise.

--- analyze.py
+++ analyze.py
@@ -77,48 +77,62 @@ def main(argv):
     db = shelve.open(args[1])
     try:
         idx = db['maxkey']
+        print('%sAppending to existing database at index %s%s' % (GREEN, idx, ENDC))
     except KeyError:
+        print('%sStarting new database%s' % (GREEN, ENDC))
         idx = u'0'
 
-    for mp3 in mp3s:
-        tags = ID3(mp3)
-        # Skip already analyzed files
-        if not options.replace and 'TXXX:mashupid' in tags: continue
-        # Skip big files
-        if os.path.getsize(mp3) > MAXSIZE: continue
+    try:
+        for mp3 in mp3s:
+            tags = ID3(mp3)
+            # Skip already analyzed files
+            if not options.replace and 'TXXX:mashupid' in tags: continue
+            # Skip big files
+            if os.path.getsize(mp3) > MAXSIZE: continue
 
-        echosong = retry(audio.LocalAnalysis, [mp3], (socket.error,), sleep=30)
-        moreinfo = track_from_id(echosong.analysis.identifier)
+            try:
+                echosong = retry(audio.LocalAnalysis, [mp3], (socket.error,), sleep=30)
+            except Exception:
+                # This is to handle the LocalAnalysis call in the event it fails for
+                # reasons out of my control. Print an error message and move on. When
+                # you have time you should change the library's source to use something
+                # smarter, say a specialized exception: AnalysisPendingException, etc.
+                sys.stderr.write('%sSong analysis failed, skipping %s%s\n' % (RED, mp3, ENDC))
+                continue
+            moreinfo = track_from_id(echosong.analysis.identifier)
 
-        # Add main tags
-        tags.add(TBPM(encoding=1, text=unicode(round(echosong.analysis.tempo['value']))))
-        try:
-            tags.add(TKEY(encoding=1, text=key(echosong)))
-        except KeyError:
-            sys.stderr.write('%sIncorrect key info; key: %d, mode: %d%s\n' %
-                    (RED, echosong.analysis.key['value'], echosong.analysis.mode['value'], ENDC))
-        tags.add(TXXX(encoding=3, desc=u'danceability', text=unicode(moreinfo.danceability)))
-        tags.add(TXXX(encoding=3, desc=u'energy', text=unicode(moreinfo.energy)))
-        tags.add(TXXX(encoding=3, desc=u'loudness', text=unicode(moreinfo.loudness)))
+            # Add main tags
+            tags.add(TBPM(encoding=1, text=unicode(round(echosong.analysis.tempo['value']))))
+            try:
+                tags.add(TKEY(encoding=1, text=key(echosong)))
+            except KeyError:
+                sys.stderr.write('%sIncorrect key info; key: %d, mode: %d%s\n' %
+                        (RED, echosong.analysis.key['value'], echosong.analysis.mode['value'], ENDC))
+            tags.add(TXXX(encoding=3, desc=u'danceability', text=unicode(moreinfo.danceability)))
+            tags.add(TXXX(encoding=3, desc=u'energy', text=unicode(moreinfo.energy)))
+            tags.add(TXXX(encoding=3, desc=u'loudness', text=unicode(moreinfo.loudness)))
 
-        # Update entry in DB if it exists, or...
-        if 'TXXX:mashupid' in tags:
-            db[tags.get('TXXX:mashupid').text[0]] = echosong
-        else: # create a new entry
-            tags.add(TXXX(encoding=3, desc=u'mashupid', text=idx))
-            db[idx] = echosong
-            idx = unicode(int(idx) + 1)
+            # Update entry in DB if it exists, or...
+            if 'TXXX:mashupid' in tags:
+                db[tags.get('TXXX:mashupid').text[0]] = echosong
+            else: # create a new entry
+                tags.add(TXXX(encoding=3, desc=u'mashupid', text=idx))
+                db[idx] = echosong
+                idx = unicode(int(idx) + 1)
 
-        tags.save()
+            tags.save()
 
-        # So we don't hammer their servers
-        print('%sFinished analyzing %s, sleeping...%s' % (GREEN, mp3, ENDC))
-        sleep(randint(0, 3))
+            # So we don't hammer their servers
+            print('%sFinished analyzing %s, sleeping...%s' % (GREEN, mp3, ENDC))
+            sleep(randint(0, 3))
+    except KeyboardInterrupt:
+        sys.stderr.write('User termination, exiting...\n')
+    finally:
+        # Update 'maxkey'.
+        db['maxkey'] = idx
 
-    # Update 'maxkey'.
-    db['maxkey'] = idx
+        db.close()
 
-    db.close()
     print('Done!')
 
 if __name__ == '__main__':
